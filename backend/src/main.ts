345db3c7cd12ba347bc2b202a91865ca51a68d8c
xiltepin <xiltepin@gmail.com>
2026-03-27 14:10:38 +0000

    Allow dynamic origins for CORS so both localhost and local network IP are accepted

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
